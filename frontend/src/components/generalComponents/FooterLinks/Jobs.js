c0e6f026ce4da7c1219cb6629242f4b87f319a29
SuperMayy <mayowashowumi@outlook.com>
2021-10-06 17:51:58 +0100

    HR Job Added

--- frontend/src/components/generalComponents/FooterLinks/Jobs.js
+++ frontend/src/components/generalComponents/FooterLinks/Jobs.js
@@ -5,8 +5,31 @@ const Jobs = () => {
         <div style={{textAlign:"center"}}>
             <br/><br/>
             <h1>Roles</h1><br/>
-            <p>We are currently not hiring for any roles.</p>
-            <br/><br/>
+            {/* <p>We are currently not hiring for any roles.</p> */}
+            <h3>Human Resources Manager</h3><br/>
+            <br/>
+            <h4><b>About the job</b></h4>
+            <p>
+              The ideal candidate will support the full scope of Human Resources responsibilities and partner 
+              with the organization on strategic initiatives. They will maintain and enhance the organization's 
+              human resources by planning, implementing, and evaluating human resources policies, programs, and 
+              practices. 
+            </p><br/>
+            <h4><b>Responsibilities</b></h4>
+            <p>
+              Recruit and Onboard new hires Conduct performance management and provide feedback Manage Payroll and benefits for employees
+            </p><br/>
+            <h4><b>Qualifications</b></h4>
+            <p>
+                <ul>
+                    <li>Bachelor's degree or relevant experience</li>
+                    <li>2+ years' experience in Human Resources</li>
+                    <li>Strong recruiting and demonstrated ability to improve talent acquisition strategies</li>
+                    <li>Demonstrated expertise training managers and employees</li>
+                    <li>Strong organizational, critical thinking, and communications skills</li>
+                    <li>Attention to detail and good judgment</li>
+                </ul>
+            </p><br/><br/>
         </div>
     )
 }
